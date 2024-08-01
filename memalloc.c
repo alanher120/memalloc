a06f13f4b01edbb454e7b134ba480d4cafd5fe29
alanher120 <34939103+alanher120@users.noreply.github.com>
2024-08-01 22:17:00 +0800

    Update memalloc.c

--- memalloc.c
+++ memalloc.c
@@ -2,24 +2,24 @@
 #include <stdlib.h>
 
 int main() {
-    size_t size = 1024 * 1024; // 每次分配 1MB
+    size_t size = 100* 1024 * 1024; // 每次分配 100MB
     void *ptr;
     size_t allocations = 0;
 
     while (1) {
         ptr = malloc(size);
         if (ptr == NULL) {
-            printf("Memory allocation failed after %zu allocations.\n", allocations);
+            printf("Memory allocation %zu MB allocations.\n", allocations * 100);
             break;
+        } else {
+            printf("Memory allocation failed after %zu MB allocations.\n", allocations * 100);
         }
         allocations++;
-
-        // 在這裡可以選擇釋放記憶體
-        free(ptr);
     }
 
     printf("Press any key to exit...\n");
     getchar(); // 等待用戶按下任意鍵
-
+    // 在這裡可以選擇釋放記憶體
+    free(ptr);
     return 0;
 }
